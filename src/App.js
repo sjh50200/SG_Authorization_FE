d912fc03560da75be2622cd0f552d19ae7d81cb3
JongHyeon <sjh2273@naver.com>
2021-12-15 14:26:42 +0900

    login page - layout done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import LoginPage from "./components/LoginPage.js/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
+        <Header />
       <Routes>
         <Route path='/' element={<LoginPage />} />
       </Routes>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../imgs/logo.png';
+import "./Header.scss";
+
+function Header() {
+    return (
+        <div className="logoLayout">
+            <NavLink to="/" style={{ width: '400px' }}>
+                <img className="logo" src={logo} />
+            </NavLink>
+        </div>
+    )
+}
+
+export default Header;
